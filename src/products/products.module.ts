eeae36e0176616c312fd11382f9460f608f5d4a8
vyke26424 <166583357+vyke26424@users.noreply.github.com>
2025-12-04 10:30:59 +0700

    Testing linked Database
    + Test database link
    + Small change to frontend
    + add IsHot colum to table Products
    => Everything work perfectly

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   imports: [],
   controllers: [ProductsController],
-  providers: [ProductsService],
+  providers: [
+    ProductsService, 
+    PrismaService 
+  ],
 })
 export class ProductsModule {}
